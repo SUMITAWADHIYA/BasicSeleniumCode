87f22fba13645d546662e86462f8c7b643ea7a99
SUMITAWADHIYA <118043056+SUMITAWADHIYA@users.noreply.github.com>
2025-09-06 12:35:51 +0530

    ex01

---
+++ src/test/java/ex01_Selenium_basic/TestSelenium01.java
@@ -0,0 +1,18 @@
+package ex01_Selenium_basic;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium01 {
+
+    @Test
+    public void verifySHMPl(){
+        //write the code  which will execute the perform the UI intercation
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://cms.shmpl.com:2030/");
+        System.out.println(driver.getSessionId());
+        System.out.println(driver.getTitle());
+
+    }
+
+}
